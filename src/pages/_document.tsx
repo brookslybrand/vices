bbba69e6e9e0f45b50a07e9e9d0ada543800b438
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-12-13 16:14:19 -0600

    Make simple form for tobacco purchases

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         {/* <body className="min-h-screen bg-gradient-to-b from-orange-50 via-pink-50 to-orange-50"> */}
         <body className="min-h-screen bg-gradient-to-b from-orange-50 to-orange-100 ">
--- src/pages/tobacco.tsx
+++ src/pages/tobacco.tsx
@@ -1,18 +1,74 @@
+import Head from 'next/head'
+
 function Tobacco() {
   return (
     <main className="flex flex-col items-center">
+      <Head>
+        <title>Add purchase</title>
+      </Head>
       <form
         className="mt-8 space-y-4 w-80"
         onSubmit={(e) => {
+          console.log('hey')
           e.preventDefault()
         }}
       >
         <h1 className="text-4xl ">Add purchase</h1>
-        {/* <label></label> */}
-        <input className="w-full" type="date" />
+        <div>
+          <Label className="text-lg" htmlFor="date">
+            Purchase date *
+          </Label>
+          <input id="date" className="w-full" type="date" required />
+        </div>
+
+        <div>
+          <Label className="text-lg" htmlFor="description">
+            Name *
+          </Label>
+          <input className="block w-full" id="name" type="text" required />
+        </div>
+
+        <div>
+          <Label className="text-lg" htmlFor="description">
+            Amount (oz) *
+          </Label>
+          <input
+            className="block w-full"
+            id="name"
+            type="number"
+            required
+            min="0"
+            step="0.1"
+          />
+        </div>
+
+        <div>
+          <Label className="text-lg" htmlFor="description">
+            Description
+          </Label>
+          <textarea className="block w-full" id="description" />
+        </div>
+
+        <div>
+          <Label className="text-lg" htmlFor="image">
+            Add image
+          </Label>
+          <input type="file" id="image" accept="image/*" />
+        </div>
+
+        <button
+          className="w-full py-2 text-2xl font-medium text-pink-100 bg-yellow-700 rounded-md"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </main>
   )
+}
+
+function Label({ htmlFor, ...props }: React.ComponentPropsWithoutRef<'label'>) {
+  return <label htmlFor={htmlFor} className="text-4xl" {...props} />
 }
 
 export default Tobacco
